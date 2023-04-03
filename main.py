1dbda46e1e801ce07089587be9c1f2a5fbca8213
liuryl <Liuryl@outlook.com>
2023-04-03 20:16:38 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    # 自动排序,集合可以自动去重
+
+    set={1,1,1,1,2,3,4,5,6,6,6,4,3,2,9,0,0,8}
+    print(set)
+#     list
+    alex=[1,2,3,4,5,9,8,7,6,5,4,3,3,3,3,3]
+    print(alex)
+    
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
